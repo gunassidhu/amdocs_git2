ac17618a959496429715faff979424518a930bb4
amdoconb003 <amdoconb003@Amdonb197.iiht.tech>
2022-07-20 16:01:57 +0530

    initial commit

---
+++ src/com/training_jenkins/ArithDemo.java
@@ -0,0 +1,15 @@
+package com.training_jenkins;
+
+public class ArithDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=8,b=4,c=0,d=0,e=0,f=0;
+		c=a+b;
+		d=a-b;
+		e=a*b;
+		f=a/b;
+		System.out.println("SUCCESS!!");
+	}
+
+}
